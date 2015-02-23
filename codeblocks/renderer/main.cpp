0d998c4726fe520b0e6bf2f715590a3d6e7fefbf
skylurker <skaldsandshadows@mail.com>
2015-02-23 22:11:44 +0400

    Got rid of the holes-in-da-line prob and the symmetry prob

--- codeblocks/renderer/main.cpp
+++ codeblocks/renderer/main.cpp
@@ -1,22 +1,47 @@
 #include "tgaimage.h"
+#include <cmath>
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-   for (int x=x0; x<=x1; x++) {
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
+{
+    bool steep = false; //steep = true if length < height
+    if (std::abs(x0-x1)<std::abs(y0-y1)) //if the line is sttep, we transpose the image
+    {
+        std::swap(x0,y0);
+        std::swap(x1,y1);
+        steep = true;
+    }
+    if (x0>x1) //to avoid the symmetry problem: line(x0,y0,x1,y1)!=line(x1,y1,x0,y0)
+    {
+        //make it left-to-right
+        std::swap(x0,x1);
+        std::swap(y0,y1);
+    }
+    for (int x=x0; x<=x1; x++)
+    {
         float t = (x-x0)/(float)(x1-x0);
         int y = y0*(1.-t) + y1*t;
-        image.set(x, y, color);
+        if(steep)
+        {
+            image.set(y, x, color); //if transposed, de-transpose
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+
     }
 }
 
 
-int main(int argc, char** argv) {
-        TGAImage image(100, 100, TGAImage::RGB);
-        image.set(52, 41, red);
-        line(20, 13, 40, 80, image, red);
-        image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
-        image.write_tga_file("output.tga");
-        return 0;
+int main(int argc, char** argv)
+{
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(52, 41, red);
+    line(20, 13, 40, 80, image, red);
+    image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
+    image.write_tga_file("output.tga");
+    return 0;
 }
