f7107e37317ce9e70a7acea1dba2c4fbb58d6129
skylurker <skaldsandshadows@mail.com>
2015-03-10 21:08:02 +0400

    Fixed a typo (- inst of = at main.cpp 61), rendered teh head

--- codeblocks/renderer/main.cpp
+++ codeblocks/renderer/main.cpp
@@ -1,8 +1,17 @@
-#include "tgaimage.h"
+#include <vector>
 #include <cmath>
+#include "tgaimage.h"
+#include "model.h"
+#include "geometry.h"
+
+
+//We're getting back from the "higher performance code" to the "readable code" here
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
+Model *model = NULL;
+const int width = 800; //pixels
+const int height = 800; //pixels
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
 {
@@ -20,43 +29,58 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color)
         std::swap(y0,y1);
     }
 
-    int dx = x1-x0;
-    int dy = y1-y0;
-    int derror2 = std::abs(dy)*2;
-   // float error = 0; //from our pixel to the perfect line
-   int error2 = 0;
-    int y = y0;
+
     for (int x=x0; x<=x1; x++)
     {
+        float t = (x-x0)/(float)(x1-x0); //again, this one is from the line equation
+        int y = y0*(1.-t) + y1*t;       //(y-y1)/(y2-y1) = (x-x1)/(x2-x1) = t
 
         if(steep)
         {
-            image.set(y, x,  TGAColor(255, 1)); //if transposed, de-transpose
+            image.set(y, x,  color); //if transposed, de-transpose
         }
         else
         {
-            image.set(x, y,  TGAColor(255, 1));
+            image.set(x, y,  color);
         }
-        error2 += derror2;
 
-        if(error2>dx){
-            y += (y1>y0?1:-1);
-            error2 -= dx*2;
-        }
 
     }
 }
 
 
 int main(int argc, char** argv)
+/* Параметр argc содержит количество аргументов
+в командной строке и является целым числом,
+причем он всегда не меньше 1, потому что первым аргументом
+считается имя программы. */
 {
-    TGAImage image(100, 100, TGAImage::RGB);
-    //image.set(52, 41, red); //a red dot!
-    for(int i=0;i<1000000;i++){
-         line(20, 13, 40, 80, image, red);
-    }
+  if (2==argc){
+    model = new Model(argv[1]); //if there is an argument which specifies the model we use
+  } else{
+    model = new Model("obj/african_head.obj"); //by default
+  }
+  TGAImage image(width, height, TGAImage::RGB);
+  for (int i=0; i<model->nfaces(); i++){
+    // a->b Обращение к члену структуры («член b объекта, на который указывает a»)
+    std::vector<int> face = model->face(i);
+    for (int j=0; j<3; j++){
+    Vec3f v0 = model->vert(face[j]);
+    Vec3f v1 = model->vert(face[(j+1)%3]);
+    int x0 = (v0.x+1.)*width/2.; //for explanation lurk below
+    int y0 = (v0.y+1.)*height/2.;
+    int x1 = (v1.x+1.)*width/2.;
+    int y1 = (v1.y+1.)*height/2.;
+    /* All the vertices appear to have values ranging from -1 to 1.
+    Thus, by adding 1 to each we get 0 to 2. Multiplying these by width
+    and dividing by 2, we get a range of 0 to width as a result. Yay! */
+    line(x0, y0, x1, y1, image, white);
+  }
+
+  }
 
     image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+    delete model; //get out from the memory, you sneaky thing
     return 0;
 }
