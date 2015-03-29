625f03e18bff380bff1e286f494503f265c91d23
skylurker <skaldsandshadows@mail.com>
2015-03-29 18:45:10 +0400

    Described the +.5 cast->rounding hack

--- codeblocks/renderer/main.cpp
+++ codeblocks/renderer/main.cpp
@@ -20,7 +20,7 @@ const int height = 500; //pixels
 void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) //vector = (x, y) a.k.a. one dot
 {
     bool steep = false; //steep = true if length < height
-    if (std::abs(p0.x - p1.x)<std::abs(p0.y - p1.y)) //if the line is sttep, we transpose the image
+    if (std::abs(p0.x - p1.x)<std::abs(p0.y - p1.y)) //if the line is steep, we transpose the image
     {
         std::swap(p0.x, p0.y);
         std::swap(p1.x, p1.y);
@@ -37,7 +37,11 @@ void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color) //vector = (x, y)
     {
         float t = (x-p0.x)/(float)(p1.x-p0.x); //again, this one is from the line equation
         int y = p0.y*(1.-t) + p1.y*t + .5;       //(y-y1)/(y2-y1) = (x-x1)/(x2-x1) = t
-        //the + .5 part was added in the 2d z-buffer learning version
+        /* the + .5 part was added in the 2d z-buffer learning version
+            this seems to be-- pardon, this IS a hack to make an ordinary float -> int
+            cast (which simply gets rid of the fractional component, i.e. 5.9 -> 5)
+            more like normal Math.round thing, i.e. 5.9 -> 6 (5.9 + 0.5 = 6.8 -> 6)
+        */
         if(steep)
         {
             image.set(y, x,  color); //if transposed, de-transpose
@@ -61,13 +65,13 @@ int main(int argc, char** argv)
 //the 2d scene which we'll have to render aka project to the "screen"
     TGAImage scene (width, height, TGAImage::RGB);
 
-    line(Vec2i(20,34), Vec2i(744, 400), scene, red);
+    line(Vec2i(20, 34), Vec2i(744, 400), scene, red);
     line(Vec2i(120, 434), Vec2i(444, 400), scene, green);
     line(Vec2i(330, 463), Vec2i(594, 200), scene, blue);
 
 //the "screen" line
 
-    line(Vec2i(10,10), Vec2i(790, 10), scene, white);
+    line(Vec2i(10, 10), Vec2i(790, 10), scene, white);
     scene.flip_vertically(); // we want to have the origin at the left bottom corner of the image
     scene.write_tga_file("scene.tga");
     //  image.flip_vertically(); // we want to have the origin at the left bottom corner of the image
